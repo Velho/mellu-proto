60a592683ca8afc2942021e8a6527cdfb319902d
Velho <Jewel960@gmail.com>
2015-04-07 02:10:18 +0300

    Event's are selected from EventTable which is defined and declared in EventTable class.

--- src/EventTable.h
+++ src/EventTable.h
@@ -1,10 +1,42 @@
 #ifndef PROTO_EVENTTABLE_H
 #define PROTO_EVENTTABLE_H
 
+#include "Event.h"
+
 namespace Proto {
 
-class EventTable {
+class GameObject;
+class EventObject;
 
+class Events;
+
+/*!
+ * \brief The EventTable class
+ * Constructs the Events at compile time for
+ * EventObjects to use.
+ */
+class EventTable {
+public:
+	using EvtPtr = std::unique_ptr<Event>;
+    EventTable(Events&);
+
+    void retrieve_events();
+
+    /*!
+     * \brief get_event
+     * Returns event out of Event table
+     * \return
+     */
+    Event *get_event(std::size_t);
+    Event *operator[](std::size_t);
+
+private:
+    std::string level;
+    Events &events;
+    std::vector<EvtPtr> event_table;
+
+    void load_evts();
+    void load_evts_proto();
 };
 }
 
