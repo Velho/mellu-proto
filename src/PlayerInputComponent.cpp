f4cc41b83c428e1690813b3ad331c22db478e604
Velho <Jewel960@gmail.com>
2015-04-13 20:11:08 +0300

    Removed unused code.

--- src/PlayerInputComponent.cpp
+++ src/PlayerInputComponent.cpp
@@ -42,42 +42,4 @@ void PlayerInputComponent::set_right(sf::Event &event)
             arrow_right = false;
     }
 }
-
-/*// BUGGED INPUT HANDLING
-void PlayerInputComponent::update(GameObject &obj, sf::Event& event)
-{
-    if(event.type == sf::Event::KeyPressed) {
-        // LEFT
-        if (event.key.code == sf::Keyboard::Left) {
-
-            if(current_keypress == KeyPress::Right) {
-                std::cout << "Left -> Right" << std::endl;
-                current_keypress = KeyPress::Right;
-            } else {
-                current_keypress = KeyPress::Left;
-                last_keypress = KeyPress::Left;
-                std::cout << "Left KeyPress" << std::endl;
-            }
-        }
-        // RIGHT
-        if (event.key.code == sf::Keyboard::Right) {
-
-            if(current_keypress == KeyPress::Left) {
-                std::cout << "Right -> Left" << std::endl;
-                current_keypress = KeyPress::Left;
-            } else {
-                current_keypress = KeyPress::Right;
-                last_keypress = KeyPress::Right;
-                std::cout << "Right KeyPress" << std::endl;
-            }
-        }
-    } else if (event.type == sf::Event::KeyReleased) {
-        if(last_keypress == KeyPress::Left && current_keypress != KeyPress::Right)
-            current_keypress = KeyPress::None;
-        if(last_keypress == KeyPress::Right && current_keypress != KeyPress::Left)
-            current_keypress = KeyPress::None;
-    }
-}
-*/
-
 }
