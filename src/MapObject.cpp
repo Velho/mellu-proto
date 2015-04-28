2e058669c7a2aa7d173cd115966dd70d5c2ff45c
Velho <Jewel960@gmail.com>
2015-04-28 17:54:56 +0300

    Added rotation to MapObjects, MapObjects are saved to file with angle.

--- src/MapObject.cpp
+++ src/MapObject.cpp
@@ -12,6 +12,11 @@ void MapObject::set_size(sf::Vector2f sz)
     shape.setSize(sz);
 }
 
+void MapObject::set_rotation(float angle)
+{
+    shape.setRotation(angle);
+}
+
 sf::Vector2f MapObject::get_position() const
 {
     return shape.getPosition();
@@ -25,6 +30,11 @@ sf::Vector2f MapObject::get_size() const
 sf::FloatRect MapObject::get_frect() const
 {
     return sf::FloatRect{ get_position(), get_size() };
+}
+
+float MapObject::get_rotation() const
+{
+    return shape.getRotation();
 }
 
 void MapObject::draw(sf::RenderTarget &target, sf::RenderStates states) const
