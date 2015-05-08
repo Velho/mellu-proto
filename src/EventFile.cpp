124211b99a23e447171cb98244ef31c7b880225f
velho <Jewel960@gmail.com>
2015-05-08 22:23:06 +0300

    Modified events table; dropped one column, modified existing code to support the change. Refactoring and architectural changes to saving the data.

--- src/EventFile.cpp
+++ src/EventFile.cpp
@@ -13,7 +13,7 @@ namespace Proto {
  */
 struct EventObjectRow {
     double x, y, width, height;
-    int evt_id, evt_type, evt_table_id;
+    int evt_id, evt_table_id;
 
     /*!
      * \brief EventObjectRow
@@ -27,9 +27,9 @@ struct EventObjectRow {
      */
     EventObjectRow(double _x, double _y,
                    double _width, double _height,
-                   int type, int eid, int id) :
+                   int eid, int id) :
         x{ _x }, y{ _y }, width{ _width }, height{ _height },
-        evt_type{ type }, evt_table_id{ eid }, evt_id{ id }
+        evt_table_id{ eid }, evt_id{ id }
     { }
 
     /*!
@@ -44,7 +44,7 @@ struct EventObjectRow {
     EventObjectRow(Query::Row row) :
         EventObjectRow(
             row.get_double(0), row.get_double(1), row.get_double(2), row.get_double(3),
-            0, row.get_int(4), row.get_int(5))
+            row.get_int(4), row.get_int(5))
     {
     }
 
@@ -78,49 +78,19 @@ std::vector<std::unique_ptr<EventObject>> EventFile::load()
 void EventFile::save(Events &evt)
 {
     Database db(filename);
+    std::ostringstream ins_sql;
 
     db.exec("DELETE FROM events;"); // Drop the table.
 
-    std::vector<EventObject*> new_objects; // New objects without index.
-    std::ostringstream ins_sql;
-    int idx{ 1 };
-
     for(auto &obj : evt.evt_objs) {
-        if(has_index(obj.get())) {
-            // Insert query.
-            ins_sql << "INSERT INTO events(evt_id, x, y, width, height, evt_table_id) VALUES(";
-            ins_sql << obj->get_evt_id() << ", " << obj->get_position().x << ", " << obj->get_position().y << ", ";
-            ins_sql << obj->get_size().x << ", " << obj->get_size().y << ", ";
-            ins_sql << obj->get_evt_table_id() << ");";
-        } else
-            new_objects.push_back(obj.get());
-
-        idx++;
+        // Insert query.
+        ins_sql << "INSERT INTO events(evt_id, x, y, width, height, evt_table_id) VALUES(";
+        ins_sql << obj->get_evt_id() << ", " << obj->get_position().x << ", " << obj->get_position().y << ", ";
+        ins_sql << obj->get_size().x << ", " << obj->get_size().y << ", ";
+        ins_sql << obj->get_evt_table_id() << ");";
     }
-
-    // Prepare objects without index.
-    insert_new_objs(new_objects, ins_sql);
 
     // Execute query.
     db.exec(ins_sql.str());
 }
-
-bool EventFile::has_index(EventObject *e_obj)
-{
-    if(e_obj->get_evt_id() != 0)
-        return true;
-
-    return false;
-}
-
-void EventFile::insert_new_objs(std::vector<EventObject*> &evt_vec, std::ostringstream &ins_sql)
-{
-    for(auto obj : evt_vec) {
-        ins_sql << "INSERT INTO events(x, y, width, height, evt_table_id) VALUES(";
-        ins_sql << obj->get_position().x << ", " << obj->get_position().y << ", ";
-        ins_sql << obj->get_size().x << ", " << obj->get_size().y << ", ";
-        ins_sql << obj->get_evt_table_id() << ");";
-    }
-}
-
 }
--- src/MapFile.cpp
+++ src/MapFile.cpp
@@ -67,45 +67,18 @@ std::vector<std::unique_ptr<MapObject>> MapFile::load()
 void MapFile::save(Map &map)
 {
     Database db(filename);
+    std::ostringstream ins_sql;
 
     db.exec("DELETE FROM map;"); // Clear the map table(Maybe not the best way...).
 
-    std::vector<MapObject*> new_objects;
-    std::ostringstream ins_sql;
-    int idx{ 1 };
-
     for(auto &obj : map.objects) {
-
-        if(has_index(obj.get())) {
-            // Construct sql statement.
-            ins_sql << "INSERT INTO map(id, x, y, width, height, angle) VALUES(";
-            ins_sql << idx << ", " << obj->get_position().x << ", " << obj->get_position().y << ", ";
-            ins_sql << obj->get_size().x << ", " << obj->get_size().y << ", " << obj->get_rotation() << ");";
-        } else
-            new_objects.push_back(obj.get());
-
-        idx++;
-    }
-
-    insert_new_objects(new_objects, ins_sql);
-    db.exec(ins_sql.str());
-}
-
-bool MapFile::has_index(MapObject *m_obj)
-{
-    if(m_obj->get_id() != 0)
-        return true;
-
-    return false;
-}
-
-void MapFile::insert_new_objects(std::vector<MapObject*> &map_vec, std::ostringstream &ins_sql)
-{
-    for(auto obj : map_vec) {
-        ins_sql << "INSERT INTO map(x, y, width, height, angle) VALUES(";
-        ins_sql << obj->get_position().x << ", " << obj->get_position().y << ", ";
+        // Construct sql statement.
+        ins_sql << "INSERT INTO map(id, x, y, width, height, angle) VALUES(";
+        ins_sql << obj->get_id() << ", " << obj->get_position().x << ", " << obj->get_position().y << ", ";
         ins_sql << obj->get_size().x << ", " << obj->get_size().y << ", " << obj->get_rotation() << ");";
     }
-}
 
+    //insert_new_objects(new_objects, ins_sql);
+    db.exec(ins_sql.str());
+}
 } // Namespace Proto
