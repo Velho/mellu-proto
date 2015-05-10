bea8b7f0b3b6854a8a80c9622afed87dca4803ea
velho <Jewel960@gmail.com>
2015-05-11 00:14:57 +0300

    RenderObject : Make use of Layout class.

--- src/RenderObject.cpp
+++ src/RenderObject.cpp
@@ -1,15 +1,55 @@
 #include "RenderObject.h"
 
+#include "GameObject.h"
+#include "EventObject.h"
+#include "MapObject.h"
+#include "Decoration.h"
+
+#include "Layout.h"
+
 namespace Proto {
 
-RenderObject::RenderObject() : draw_object{ nullptr }, priority{ 0 }
+RenderObject::RenderObject() :
+    draw_object{ nullptr },
+    priority{ 0 },
+    type{ RenderType::None }
 {
-
 }
 
-RenderObject::RenderObject(const GameObject &g_obj)
+RenderObject::RenderObject(
+        const GameObject *g_obj,
+        const Layout *lay,
+        int p) :
+    draw_object{ g_obj }, layout{ lay },
+    priority{ p }, type{ RenderType::Game }
 {
+}
 
+RenderObject::RenderObject(
+        const EventObject *e_obj,
+        const Layout *lay,
+        int p) :
+    draw_object{ e_obj }, layout{ lay },
+    priority{ p }, type{ RenderType::Event }
+{
+}
+
+RenderObject::RenderObject(
+        const MapObject *m_obj,
+        const Layout *lay,
+        int p) :
+    draw_object{ m_obj }, layout{ lay },
+    priority{ p }, type{ RenderType::Map }
+{
+}
+
+RenderObject::RenderObject(
+        const Decoration *deco_obj,
+        const Layout *lay,
+        int p) :
+    draw_object{ deco_obj }, layout{ lay },
+    priority{ p }, type{ RenderType::Decoration }
+{
 }
 
 }
--- src/RenderObject.h
+++ src/RenderObject.h
@@ -5,42 +5,74 @@
 
 namespace Proto {
 
-/*!
- *\brief Importance enum class
- * Priorities RenderObject into three
- * different categories. 
- */
-enum class Importance {
-    Low, Medium, High
-};
-
 // Classes that are drawn.
 class GameObject;
 class MapObject;
 class EventObject;
+class Decoration;
+
+class Layout;
+
+/*!
+ *\brief RenderObject enum class
+ * Enumerates different ObjectTypes.
+ * Used in the layout table.
+ */
+enum class RenderType {
+    Map, Event, Game, Decoration, None
+};
 
 /*!
  *\brief RenderObject class
  * Wraps any sf::Drawable object into
  * pointer for controlling the Rendering
  * process. Template pointer for each object?
- *
+ * 
  * std::vector<RenderObject> renderables;
+ * std::sort(renderable);
  * void draw(sf::RenderTarget &target) {
  * 
  * }
+ *
+ * RenderObject is represented as layout table
+ * in the maps database.
  */
 class RenderObject {
 public:
+    /*!
+     *\brief
+     */
     RenderObject();
+    /*!
+     *\brief
+     */
+    RenderObject(const GameObject*, const Layout*, int);
+    /*!
+     *\brief
+     */
+    RenderObject(const MapObject*, const Layout*, int);
+    /*!
+     *\brief
+     */
+    RenderObject(const EventObject*, const Layout*, int);
+    /*!
+     *\brief
+     */
+    RenderObject(const Decoration*, const Layout*, int);
 
-    RenderObject(const GameObject&);
-    RenderObject(const MapObject&);
-    RenderObject(const EventObject&);
+    const sf::Drawable &operator()(){return *draw_object;}
+    const sf::Drawable *get_drawable() const { return draw_object; }
+
+    Layout const *get_layout() const { return layout; }
+
+    int get_priority() const { return priority; }
+    RenderType get_type() const { return type; }
 
 private:
-    sf::Drawable *draw_object;
+    const sf::Drawable *draw_object;
+    const Layout *layout;
     int priority;
+    RenderType type;
 };
 
 }
